81ef9b3c1c339a9969f23d605c0e56821278c5c5
xtinekim <christine.kim@hotmail.ca>
2016-08-15 13:48:15 -0400

    Added slider coding
    Added the slider coding for image to show. Not done editing

--- index.php
+++ index.php
@@ -9,48 +9,89 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package Portfolio
+ * @package ckim-assignment 1/Portfolio
  */
 
 get_header(); ?>
 
+    <figure class="snip1543">
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wifehusband.jpg" alt="Banner" />
+  <figcaption>
+    <h3>Inverness McKenzie</h3>
+    <p>The only skills I have the patience to learn are those that have no real application in life.</p>
+  </figcaption>
+  <a href="#"></a>
+</figure>
+
+<figure class="snip1543 hover">
+<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ring.jpg" alt="Banner" />
+  <figcaption>
+    <h3>Alan Fresco</h3>
+    <p>The real fun of living wisely is that you get to be smug about it.</p>
+  </figcaption>
+  <a href="#"></a>
+</figure>
+
+<figure class="snip1543">
+<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bouquet.jpg" alt="Banner" />
+  <figcaption>
+    <h3>Indigo Violet</h3>
+    <p>But Calvin is no kind and loving god! He's one of the old gods! He demands sacrifice! </p>
+  </figcaption>
+  <a href="#"></a>
+</figure>
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+
+<figure class="snip1543">
+<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/love.jpg" alt="Banner" />
+  <figcaption>
+    <h3>Indigo Violet</h3>
+    <p>But Calvin is no kind and loving god! He's one of the old gods! He demands sacrifice! </p>
+  </figcaption>
+  <a href="#"></a>
+</figure>
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+
+<figure class="snip1543">
+<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/happily.jpg" alt="Banner" />
+  <figcaption>
+    <h3>Indigo Violet</h3>
+    <p>But Calvin is no kind and loving god! He's one of the old gods! He demands sacrifice! </p>
+  </figcaption>
+  <a href="#"></a>
+</figure>
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+
+        <script src="js/slider.js"></script>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) :
+		<?php if ( have_posts() ) : ?>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php
-			endif;
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<?php endwhile; ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+			<?php the_posts_navigation(); ?>
 
-			endwhile;
+		<?php else : ?>
 
-			the_posts_navigation();
+			<?php get_template_part( 'content', 'none' ); ?>
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
