81682ffcb0070de8d0c36a788086de18b085eb15
xtinekim <christine.kim@hotmail.ca>
2016-08-19 16:18:46 -0400

    Final edits

--- slider.js
+++ slider.js
@@ -1,3 +1,11 @@
+/**
+ * The is the javascript for the slider
+ *
+ * Reference: Code Pen
+ *
+ * @package ckim-assignment1/Portfolio
+ */
+
 $(".hover").mouseleave(
   function() {
     $(this).removeClass("hover");
