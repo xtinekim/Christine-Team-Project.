aadb91eea2858ad63b80e4b5b7f8761c16858ae4
xtinekim <christine.kim@hotmail.ca>
2016-08-19 02:28:22 -0400

    Edited
    Slider and Carousel

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 /**
  * The main template file.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package ckim-assignment 1/Portfolio
+ * @package ckim-assignment1/Portfolio
  */
 
 get_header(); ?>
@@ -68,7 +64,7 @@ get_header(); ?>
 
 			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+	
 
 		<?php else : ?>
 
