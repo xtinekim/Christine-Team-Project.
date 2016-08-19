eace56207860e5cbfee1edde5938d4943b917464
xtinekim <christine.kim@hotmail.ca>
2016-08-19 02:35:05 -0400

    Added
    Reference: Portfolio.
    Added Style for Options Page.

--- footer.php
+++ footer.php
@@ -2,16 +2,35 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Portfolio
+ * @package ckim-assignment1/Portfolio
  */
+
+$options=get_option( 'ck_options_settings' ); 
 
 ?>
 
 	</div><!-- #content -->
+
+	<!-- This is the style for my options  -->
+     <style>
+        #colophon .site-info{
+	       background-color: <?php echo $options['ck_radio_field']; ?>
+        }
+    </style>
+
+    <style>
+        #colophon .site-info{
+	       color: <?php echo $options['ck_select_field']; ?>
+        }
+    </style>
+
+<?php if(! is_active_sidebar('sidebar-1')){
+	return;
+}
+?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
@@ -19,6 +38,10 @@
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'portfolio' ), 'portfolio', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
+ 
+
+
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
